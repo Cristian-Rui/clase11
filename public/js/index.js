8548fb488c0f861ed714dbae37a80d5f6201ecfa
Cristian Rui <cristian.rui98@gmail.com>
2023-12-19 14:53:08 -0300

    firt commit

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+const socket = io();
+let userName;
+
+Swal.fire({
+    title: "INGRESE SU NOMBRE",
+    input: "text",
+    inputValidator: (value) => {
+        if (!value) {
+            return 'Tienes que ingresar tu nombre';
+        }
+    }
+}).then(data => {
+    userName = data.value;
+    socket.emit('newUser', userName);
+});
+
+const inputData = document.getElementById('inputData');
+const outputData = document.getElementById('outputData');
+
+inputData.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        if (!!inputData.value.trim()) {
+            socket.emit('message', { user: userName, data: inputData.value })
+        }
+    }
+})
+
+socket.on('messagesLogs', data => {
+    let messages = '';
+    data.forEach(message => {
+        messages += `${message.user} dice ${message.data} <br />`
+    })
+
+    outputData.innerHTML = messages;
+})
+
+socket.on('newConnection', data => {
+    console.log(data);
+})
+
+socket.on('notification', user => {
+    Swal.fire({
+        position: "top-end",
+        toast: true,
+        text: `el usuario ${user} se ha conectado`,
+        timer: 1500
+    });
+})
